dd95325805caad901caf454c4f4cde89e50e74c8
Diarmuid Wrenne <diarmuid@bluekulu.com>
2012-11-03 14:44:57 +0000

    include psycopg in reqs file

--- lessongen/lessongen/apps/generator/urls.py
+++ lessongen/lessongen/apps/generator/urls.py
@@ -3,6 +3,7 @@ from django.views.generic.simple import direct_to_template
 
 
 urlpatterns = patterns("",
+                       url(r"^$",'generator.views.gen_simple_math', name='gen_simple_math'),
     url(r'^simple_math/$', 'generator.views.gen_simple_math', name='gen_simple_math'),
 
 )
